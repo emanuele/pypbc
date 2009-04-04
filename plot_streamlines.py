d22aebe5c7c705ad18a876e5924e6fefa161a8f3
Emanuele Olivetti <emanuele@relativita.com>
2009-04-05 01:04:52 +0200

    NF: added first example on plotting streamlines, with mayavi2.

---
+++ plot_streamlines.py
@@ -0,0 +1,37 @@
+"""Plotting streamlines with mayavi2.
+
+Note that you need a recent mayavi2 in order to run this. If you
+use Ubuntu Hardy Heron 8.04 LTS like me, have a look here:
+http://gael-varoquaux.info/blog/?p=70
+and use these repositories:
+https://launchpad.net/~gael-varoquaux/+archive
+Note that mayavi2 shipped with more recent releases of Ubuntu works
+fine.
+"""
+
+from enthought.mayavi import mlab
+import streamlines
+
+
+if __name__=="__main__":
+
+    # filename = "DeterministicTractography/QBALLRecon/hardiO10.trk"
+    filename = "hardiO10.trk_cross_streamline_id_20.trk"
+
+    
+    try:
+        s
+    except:
+        s = streamlines.Streamlines()
+        print
+        print "file:", filename
+        s.loadTrk(filename)
+        s.printHeaderTrk()
+        pass
+
+    for stream, tmp in s.streamline:
+        mlab.plot3d(stream[:,0], stream[:,1], stream[:,2], tube_radius=0.2)
+        pass
+
+    
+    mlab.show()
